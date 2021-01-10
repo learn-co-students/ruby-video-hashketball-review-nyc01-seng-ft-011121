6a021b8ae97f866f6667153fbea1c0f509def932
Dayne Dewar <daynedewar@yahoo.com>
2021-01-10 21:36:09 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,5 @@
 # Write your code below game_hash
-
+require 'pry'
 def game_hash
   {
     home: {
@@ -127,4 +127,121 @@ def game_hash
   }
 end
 
-# Write code here
+def num_points_scored(player)
+  points = 0
+  game_hash.each do |k, v|
+    v[:players].each do |starting_five|
+      if starting_five[:player_name] == player
+        points = starting_five[:points]
+      end
+    end
+  end
+  points
+end
+
+def shoe_size(player)
+  shoe = 0
+  game_hash.each do |k, v|
+    v[:players].each do |starting_five|
+      if starting_five[:player_name] == player
+        shoe = starting_five[:shoe]
+      end
+    end
+  end
+  shoe
+end
+
+def team_colors(team)
+  color = ""
+  game_hash.each do |k, v|
+    if v[:team_name] == team
+      color = v[:colors]
+    end
+  end
+  color
+end
+
+def team_names
+  teams = [] 
+  game_hash.each do |k, v|
+    teams << v[:team_name]
+  end
+  teams
+end
+
+def player_numbers(team)
+  jersey_numbers =[]
+  game_hash.each do |k, v|
+    if v[:team_name] == team
+      v[:players].each do |starting_five|
+        jersey_numbers << starting_five[:number]
+      end
+    end
+  end
+  jersey_numbers
+end
+
+def player_stats(player)
+  stats = {}
+  game_hash.each do |k, v|
+    v[:players].each do |starting_five|
+      if starting_five[:player_name] == player
+        stats = starting_five
+      end
+    end
+  end
+  stats
+end
+
+def big_shoe_rebounds
+  shoe = 0
+  rebound = 0
+  game_hash.each do |k, v|
+    v[:players].each do |starting_five|
+      if starting_five[:shoe] > shoe
+        shoe = starting_five[:shoe]
+      end
+    end
+    v[:players].each do |starting_five|
+      if starting_five[:shoe] == shoe
+        rebound = starting_five[:rebounds]
+      end
+    end
+  end
+  rebound
+end
+
+def most_points_scored
+  points = 0
+  most_point_player = ""
+  game_hash.each do |k, v|
+    v[:players].each do |starting_five|
+      if starting_five[:points] > points
+        points = starting_five[:points]
+      end
+    end
+    v[:players].each do |starting_five|
+      if starting_five[:points] == points
+        most_point_player = starting_five[:player_name]
+      end
+    end
+  end
+  most_point_player
+end
+
+def winning_team
+  home_team_points = 0
+  away_team_points = 0
+  
+  game_hash[:home][:players].each do |starting_five|
+    home_team_points = starting_five[:points] + home_team_points
+  end
+  game_hash[:away][:players].each do |starting_five|
+    away_team_points = starting_five[:points] + away_team_points
+  end
+  if home_team_points > away_team_points
+    game_hash[:home][:team_name]
+    else
+    game_hash[:away][:team_name]
+  end
+end
